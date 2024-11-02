a0232c33a78a6b7c6f3fb23fbb8944975f6277fa
TOM <bitebem@gmail.com>
2024-11-02 10:21:48 +0700

    scene

--- script/ui.gd
+++ script/ui.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_top_down_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/TopDown_world.tscn")
+
+
+func _on_paltformer_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/paltformer_world.tscn")
